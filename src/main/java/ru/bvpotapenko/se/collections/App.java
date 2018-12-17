f0c1aead75c2d0f9bd698c8fa72b005664aab2f9
bvpotapenko <bvpotapenko@gmail.com>
2018-12-17 10:59:20 +0800

    Production test

--- src/main/java/ru/bvpotapenko/se/collections/App.java
+++ src/main/java/ru/bvpotapenko/se/collections/App.java
@@ -1,13 +1,81 @@
 package ru.bvpotapenko.se.collections;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+// ArrayList add
+        long startTime = System.nanoTime();
+
+        for (int i = 0; i < 100000; i++) {
+            arrayList.add(i);
+        }
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+        System.out.println("ArrayList add:  " + duration);
+
+// LinkedList add
+        startTime = System.nanoTime();
+
+        for (int i = 0; i < 100000; i++) {
+            linkedList.add(i);
+        }
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        System.out.println("LinkedList add: " + duration);
+
+// ArrayList get
+        startTime = System.nanoTime();
+
+        for (int i = 0; i < 10000; i++) {
+            arrayList.get(i);
+        }
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        System.out.println("ArrayList get:  " + duration);
+
+// LinkedList get
+        startTime = System.nanoTime();
+
+        for (int i = 0; i < 10000; i++) {
+            linkedList.get(i);
+        }
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        System.out.println("LinkedList get: " + duration);
+
+
+
+// ArrayList remove
+        startTime = System.nanoTime();
+
+        for (int i = 9999; i >=0; i--) {
+            arrayList.remove(i);
+        }
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        System.out.println("ArrayList remove:  " + duration);
+
+
+
+// LinkedList remove
+        startTime = System.nanoTime();
+
+        for (int i = 9999; i >=0; i--) {
+            linkedList.remove(i);
+        }
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        System.out.println("LinkedList remove: " + duration);
     }
 }
