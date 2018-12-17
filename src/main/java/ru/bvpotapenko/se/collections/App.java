01f2038d43a76f278f35554fde9a7bd1bef5f68b
bvpotapenko <bvpotapenko@gmail.com>
2018-12-17 10:54:42 +0800

    Initial commit

---
+++ src/main/java/ru/bvpotapenko/se/collections/App.java
@@ -0,0 +1,13 @@
+package ru.bvpotapenko.se.collections;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
